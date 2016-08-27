ff5919a28575391055ab416fd98acdb93123830e
lpdev <lpdev@luispaulo>
2016-08-27 09:47:12 -0300

    State Registration MG, DF

---
+++ src/StateRegistration/DistritoFederal.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Brazanation\Documents\StateRegistration;
+
+use Brazanation\Documents\DigitCalculator;
+
+class DistritoFederal extends State
+{
+
+    const LABEL = 'DistritoFederal';
+    
+    const REGEX = '/^(\d{2})(\d{6})(\d{3})(\d{2})$/';
+    
+    const FORMAT = '$1.$2.$3-$4';
+    
+    const LENGTH = 13;
+    
+    const DIGITS_COUNT = 2;
+
+    public function __construct()
+    {
+        parent::__construct(self::LABEL, self::LENGTH, self::DIGITS_COUNT, self::REGEX, self::FORMAT);
+    }
+
+    public function calculateDigit($baseNumber)
+    {
+        $calculator = new DigitCalculator($baseNumber);
+        $calculator->useComplementaryInsteadOfModule();
+        $calculator->replaceWhen('0', 10, 11);
+        $calculator->withModule(DigitCalculator::MODULE_11);
+        $firstDigit = $calculator->calculate();
+        $calculator->addDigit($firstDigit);
+        $secondDigit = $calculator->calculate();
+
+        return "{$firstDigit}{$secondDigit}";
+    }
+}
---
+++ tests/StateRegistration/MinasGeraisTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Brazanation\Documents\Tests\StateRegistration;
+
+use Brazanation\Documents\StateRegistration;
+use Brazanation\Documents\StateRegistration\MinasGerais as MG;
+use Brazanation\Documents\Tests\DocumentTestCase;
+
+class MinasGerais extends DocumentTestCase
+{
+
+    public function createDocument($number)
+    {
+        return new StateRegistration($number, new MG());
+    }
+
+    public function provideValidNumbers()
+    {
+        return [
+            ['062.307.904/0081'],
+            ['208.018.974/6429'],
+            ['755.108.418/0776'],
+            ['1574297782110']
+        ];
+    }
+
+    public function provideValidNumbersAndExpectedFormat()
+    {
+        return [
+            ['2080189746429', '208.018.974/6429'],
+            ['1574297782110', '157.429.778/2110']
+        ];
+    }
+
+    public function provideEmptyData()
+    {
+        return [
+            [MG::LABEL, 0],
+            [MG::LABEL, ''],
+            [MG::LABEL, null],
+        ];
+    }
+
+    public function provideInvalidNumber()
+    {
+        return [
+            [MG::LABEL, 1],
+            [MG::LABEL, '9987477353930'],
+        ];
+    }
+}
