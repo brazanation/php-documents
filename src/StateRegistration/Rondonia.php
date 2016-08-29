d44497597d50c559c176cc1097198ad5cdd6d712
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-08-29 10:47:01 -0300

    add Rondonia StateRegistration document

---
+++ src/StateRegistration/Rondonia.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Brazanation\Documents\StateRegistration;
+
+use Brazanation\Documents\DigitCalculator;
+
+class Rondonia extends State
+{
+    const LABEL = 'Rondonia';
+
+    const REGEX = '/^(\d{13})(\d{1})$/';
+
+    const FORMAT = '$1-$2';
+
+    const LENGTH = 14;
+
+    const DIGITS_COUNT = 1;
+
+    public function __construct()
+    {
+        parent::__construct(self::LABEL, self::LENGTH, self::DIGITS_COUNT, self::REGEX, self::FORMAT);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see http://www.sintegra.gov.br/Cad_Estados/cad_RS.html
+     */
+    public function calculateDigit($baseNumber)
+    {
+        $calculator = new DigitCalculator($baseNumber);
+        $calculator->useComplementaryInsteadOfModule();
+        $calculator->replaceWhen('0', 10);
+        $calculator->replaceWhen('1', 11);
+        $calculator->withModule(DigitCalculator::MODULE_11);
+
+        $digit = $calculator->calculate();
+
+        return "{$digit}";
+    }
+}
---
+++ tests/StateRegistration/RondoniaTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Brazanation\Documents\Tests\StateRegistration;
+
+use Brazanation\Documents\StateRegistration;
+use Brazanation\Documents\StateRegistration\Rondonia;
+use Brazanation\Documents\Tests\DocumentTestCase;
+
+class RondoniaTest extends DocumentTestCase
+{
+    public function createDocument($number)
+    {
+        return new StateRegistration($number, new Rondonia());
+    }
+
+    public function provideValidNumbers()
+    {
+        return [
+            ['0000000172159-3'],
+            ['0000000172158-5'],
+            ['0000000172157-7'],
+            ['0000000172109-7'],
+            ['0000000172156-9'],
+            ['0000000172154-2'],
+            ['0000000172155-1'],
+            ['0000000172153-4'],
+            ['0000000172152-6'],
+            ['0000000172151-8'],
+            ['0000000172128-3'],
+            ['0000000172126-7'],
+            ['0000000172150-0'],
+            ['0000000172149-6'],
+            ['0000000172148-8'],
+            ['0000000172147-0'],
+            ['0000000172146-1'],
+            ['0000000172145-3'],
+            ['0000000058712-5'],
+            ['0000000172131-3'],
+            ['0000000043700-0'],
+            ['0000000050046-1'],
+            ['0000000058712-5'],
+            ['0000000172131-3'],
+            ['0000000172144-5'],
+            ['0000000172143-7'],
+            ['0000000011784-6'],
+            ['0000000002146-6']
+        ];
+    }
+
+    public function provideValidNumbersAndExpectedFormat()
+    {
+        return [
+            ['00000001721593', '0000000172159-3'],
+        ];
+    }
+
+    public function provideEmptyData()
+    {
+        return [
+            [Rondonia::LABEL, ''],
+        ];
+    }
+
+    public function provideInvalidNumber()
+    {
+        return [
+            [Rondonia::LABEL, '1111111111'],
+            [Rondonia::LABEL, '00000000021464'],
+        ];
+    }
+}
