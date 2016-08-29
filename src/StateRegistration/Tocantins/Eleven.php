f93072a29a433ea42c3952ea9d4a787a1422a1ed
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-08-29 15:13:43 -0300

    add Tocantins StateRegistration document

---
+++ src/StateRegistration/Tocantins/Eleven.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Brazanation\Documents\StateRegistration\Tocantins;
+
+use Brazanation\Documents\DigitCalculator;
+use Brazanation\Documents\StateRegistration\State;
+
+class Eleven extends State
+{
+    const LABEL = 'Tocantins';
+
+    const REGEX = '/^(\d{2})(\d{2})(\d{3})(\d{3})(\d{1})$/';
+
+    const FORMAT = '$1.$2.$3.$4-$5';
+
+    const LENGTH = 11;
+
+    const DIGITS_COUNT = 1;
+
+    public function __construct()
+    {
+        parent::__construct(self::LABEL, self::LENGTH, self::DIGITS_COUNT, self::REGEX, self::FORMAT);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function extractBaseNumber($number)
+    {
+        $baseNumber = parent::extractBaseNumber($number);
+
+        return $this->removeFixedDigits($baseNumber);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see http://www.sintegra.gov.br/Cad_Estados/cad_TO.html
+     */
+    public function calculateDigit($baseNumber)
+    {
+        $calculator = new DigitCalculator($baseNumber);
+        $calculator->useComplementaryInsteadOfModule();
+        $calculator->replaceWhen('0', 10, 11);
+
+        $digit = $calculator->calculate();
+
+        return "{$digit}";
+    }
+
+    /**
+     * Removes digits should not goes to calculate digit.
+     *
+     * They digits are in position 3 and 4.
+     *
+     * @param string $baseNumber Base number.
+     *
+     * @return string Returns base number without fixed digits.
+     */
+    private function removeFixedDigits($baseNumber)
+    {
+        return substr($baseNumber, 0, 2) . substr($baseNumber, 4);
+    }
+}
---
+++ src/StateRegistration/Tocantins/Nine.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Brazanation\Documents\StateRegistration\Tocantins;
+
+use Brazanation\Documents\DigitCalculator;
+use Brazanation\Documents\StateRegistration\State;
+
+class Nine extends State
+{
+    const LABEL = 'Tocantins';
+
+    const REGEX = '/^(\d{2})(\d{3})(\d{3})(\d{1})$/';
+
+    const FORMAT = '$1.$2.$3-$4';
+
+    const LENGTH = 9;
+
+    const DIGITS_COUNT = 1;
+
+    public function __construct()
+    {
+        parent::__construct(self::LABEL, self::LENGTH, self::DIGITS_COUNT, self::REGEX, self::FORMAT);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see http://www.sintegra.gov.br/Cad_Estados/cad_TO.html
+     */
+    public function calculateDigit($baseNumber)
+    {
+        $calculator = new DigitCalculator($baseNumber);
+        $calculator->useComplementaryInsteadOfModule();
+        $calculator->replaceWhen('0', 10, 11);
+
+        $digit = $calculator->calculate();
+
+        return "{$digit}";
+    }
+}
