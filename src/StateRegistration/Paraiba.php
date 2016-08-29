f649246978cd1bc956d66419c05a1460a30e4a1f
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-08-28 22:38:55 -0300

    add Paraiba StateRegistration document

---
+++ src/StateRegistration/Paraiba.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Brazanation\Documents\StateRegistration;
+
+use Brazanation\Documents\DigitCalculator;
+
+class Paraiba extends State
+{
+    const LABEL = 'Paraiba';
+
+    const REGEX = '/^(\d{2})(\d{3})(\d{3})(\d{1})$/';
+
+    const FORMAT = '$1.$2.$3-$4';
+
+    const LENGTH = 9;
+
+    const DIGITS_COUNT = 1;
+
+    public function __construct()
+    {
+        parent::__construct(self::LABEL, self::LENGTH, self::DIGITS_COUNT, self::REGEX, self::FORMAT);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see http://www.sintegra.gov.br/Cad_Estados/cad_PB.html
+     */
+    public function calculateDigit($baseNumber)
+    {
+        $calculator = new DigitCalculator($baseNumber);
+        $calculator->useComplementaryInsteadOfModule();
+        $calculator->replaceWhen('0', 10, 11);
+        $calculator->withModule(DigitCalculator::MODULE_11);
+        $digit = $calculator->calculate();
+
+        return "{$digit}";
+    }
+}
